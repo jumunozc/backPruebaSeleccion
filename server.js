e3126928db1497c276dfd1c9394ba3f8fd406a56
jumunozc <48154420+jumunozc@users.noreply.github.com>
2023-01-06 09:47:04 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var cors = require('cors');
+var app = express();
+const mongoose = require('mongoose');
+const Resource = require('resourcejs');
+
+var UserModel = require('./src/Models/UserModel')
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors());
+
+
+
+
+//#region  AREA DE BASE DE DATOS DE MONGO
+
+mongoose
+    .connect("mongodb+srv://jmunoz:!QAZxsw2@testseleccion.0jfdxzz.mongodb.net/?retryWrites=true&w=majority", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
+
+Resource(app, '', 'users', UserModel).rest();
+
+//#endregion
+
+//#region AREA DE INICIALIZACION DE PUERTOS
+
+const securePort = process.env.PORT || '6000';
+app.listen(securePort, () => {
+    console.log(`La api está escuchando en el puerto ${securePort}`);
+})
+
+module.exports = app;
+
+//#endregion
